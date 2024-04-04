3f6f69b4877eff5ee2575290b8bda826ab952c50
xiF616 <180400511@stu.hit.edu.cn>
2024-04-04 21:02:45 +0800

    本次提交涉及很多很多地方，包括逻辑和bug，已知的内容中只剩下读取实验结果并显示的部分了

--- display.py
+++ display.py
@@ -7,6 +7,8 @@ def launch():
 
 @app.route('/panel')
 def panel():
+    if 'username' not in session:
+        return login()
     return render_template('panel.html')
 
 @app.route('/login')
@@ -15,9 +17,8 @@ def login():
 
 @app.route('/logout', methods=['GET'])
 def logout():
-    session.pop('username')
-    if session.get('compose'):
-        session.pop('compose')
+    session.pop('username', None)
+    session.pop('compose', None)
     return render_template('main.html')
 
 @app.route('/login/check', methods=['POST'])
@@ -28,17 +29,17 @@ def check():
     else:
         return {'success': False}
 
-# 非空字符串代表跳转
 @app.route('/run', methods=['POST'])
 def run():
     print(request.form)
     dataset = ('BNCI2014001.zip', 'BNCI2014004.zip')
-    if( (request.form['source'] not in dataset) or (request.form['target'] not in dataset)):
-        return ''  # 返回为空串, 假
+    if (request.form['source'] not in dataset) or (request.form['target'] not in dataset):
+        return '未识别的数据集，请确认'  # 数据集无效
+    elif '' in request.form.values():
+        return '存在未选择的参数'
     else:
         session['compose'] = request.form
         return 'true'  # 返回有效
-    
 
 @app.route('/comparision', methods=['GET'])
 def comparision():
@@ -49,7 +50,9 @@ def framework():
 
 @app.route('/result', methods=['GET'])
 def result():
-    if not session.get('compose'):
+    if 'username' not in session:
+        return login()
+    if 'compose' not in session:
         return panel()
     print(type(session['compose']))
     sss = ext(session['compose'])
@@ -65,59 +68,42 @@ def table():
              {'head':'accuracy', 's0':65.667, 's1':55.853,'s2':65.667, 's3':55.853,'s4':65.667, 's5':55.853,'s6':65.667, 's7':55.853,'s8':65.667, 'Average':55.853,'Standard deviation':1.224},
              {'head':'accuracy', 's0':65.667, 's1':55.853,'s2':65.667, 's3':55.853,'s4':65.667, 's5':55.853,'s6':65.667, 's7':55.853,'s8':65.667, 'Average':55.853,'Standard deviation':1.224}]}
 
-def ext (my_dict):
-     # 解析
-    res = ''
-    for i in range (8):
-        if (i==0):
-            if(my_dict['task'] == '脑电帽迁移'):
-                res += '1_'
-            if(my_dict['task'] == '模态迁移'):
-                res += '2_'
-            if(my_dict['task'] == '范式迁移'):
-                res += '3_'
-        if (i==1):
-            res += my_dict['model']
-            res += '_'
-            print('res', res)
+'''
+解析
+my_dict = {
+    'task': '脑电帽迁移',
+    'model': 'XXX',
+    'epoch': '50',
+    'source': 'BNCI2014001.zip',
+    'target': 'BNCI2014002.zip',
+    'batchsize': '8',
+    'optimizer': 'SGD',
+    'ea': 'true'
+}
+ext(my_dict)
+'''
+def ext(my_dict):
+    print(my_dict)
+    if my_dict['task'] == '脑电帽迁移':
+        res = '1_'
+    elif my_dict['task'] == '模态迁移':
+        res = '2_'
+    elif my_dict['task'] == '范式迁移':
+        res = '3_'
 
-        if (i==2):
-            res += my_dict['epoch']
-            res += '_'
-
-        if (i==3):
-            res += my_dict['source'][0:11]
-            res += '_'
-
-        if (i==4):
-            res += my_dict['target'][0:11]
-            res += '_'
-
-        if (i==5):
-            res += my_dict['batchsize']
-            res += '_'
-
-        if (i==6):
-            res += my_dict['optimizer']
-            res += '_'
-
-        if (i==7):
-            if (my_dict['ea'] == 'on'):
-                res += 'EA'
-            else:
-                res += 'woEA'
+    res += my_dict['model'] + '_' \
+        + my_dict['epoch'] + '_' \
+        + my_dict['source'][0:11] + '_' \
+        + my_dict['target'][0:11] + '_' \
+        + my_dict['batchsize'] + '_' \
+        + my_dict['optimizer'] + '_'
+    
+    if my_dict['ea'] == 'true':
+        res += 'EA'
+    elif my_dict['ea'] == 'false':
+        res += 'woEA'
+    
     return res
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080, threaded=True)  # host='0.0.0.0'
-    # my_dict = {
-    #     'task': '脑电帽迁移',
-    #     'model': 'XXX',
-    #     'epoch': '50',
-    #     'source': 'BNCI2014001.zip',
-    #     'target': 'BNCI2014002.zip',
-    #     'batchsize': '8',
-    #     'optimizer': 'SGD',
-    #     'ea': 'on'
-    # }
-    # ext(my_dict)
